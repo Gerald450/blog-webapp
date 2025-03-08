516e5025e43074d05a1953ba0d5777092933c56c
Gerald Shimo <shimogerald450@gmail.com>
2025-03-07 23:09:34 -0600

    First commit

---
+++ index.js
@@ -0,0 +1,35 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+
+const app = express();
+const port = 3000;
+let list = [];
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.post("/", (req, res) => {
+    list.push(req.body);
+    res.render('index.ejs', {listy: list,})
+});
+
+app.post('/delete', (req, res) => {
+    list.splice(req.body['index'],1);
+    res.render('index.ejs', {listy: list})
+})
+
+app.post('/update', (req, res) =>{
+    let index = req.body['index'];
+    list[index] = {title: req.body['title'], des: req.body['des']};
+    res.render('index.ejs', {listy: list});
+})
+
+app.listen(port, () => {
+  console.log(`listening at port ${port}`);
+});
